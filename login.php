ffe590ae83705c2ca8b926f803aaf52cebef905e
kznac <egorbarbalat007@gmail.com>
2026-01-18 13:32:34 +0100

    Refactor login form structure and update CSS for improved layout and alignment

--- login.php
+++ login.php
@@ -8,9 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <?php require('partials/head.php')?>
     <?php require('partials/nav.php')?>
-    <form method="POST">
-        <input type="text" name="username" placeholder="login">
-        <input type="password" name="password" placeholder="password">
-        <button type="submit">Log In</button>
-    </form>
+    <fieldset>
+        <legend>Log in</legend>
+        <form method="POST">
+            <input type="text" name="username" placeholder="login">
+            <input type="password" name="password" placeholder="password">
+            <button type="submit">Log In</button>
+        </form>
+    </fieldset>
 <?php require('partials/footer.php')?>
