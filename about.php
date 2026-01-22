26b8ed50e57bf51ba924a9be353e618f642fba87
kznac <egorbarbalat007@gmail.com>
2026-01-22 20:36:05 +0100

    Add heading to about page

--- about.php
+++ about.php
@@ -3,4 +3,5 @@ session_start();
 ?>
 <?php require('partials/head.php')?>
 <?php require('partials/nav.php')?>
+    <h1>A SIMPLE PHP GUESTBOOK APPLICATION WITH USER AUTHENTICATION AND MESSAGE POSTING FUNCTIONALITY.</h1>
 <?php require('partials/footer.php')?>
